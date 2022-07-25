f45151c04e7a517ed3291c35f249456dd7a694de
matheusunb <alextexugo@gmail.com>
2022-07-25 00:19:09 -0300

    aula 3

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,12 +4,20 @@ const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...`
 
+    let valor = 'Matheus Lima'
+
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
     return(
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
