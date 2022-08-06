dbaee2a084eb5b31d958272422a8b9d754df2575
matheusunb <alextexugo@gmail.com>
2022-08-06 12:55:04 -0300

    aula 4

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,12 +4,8 @@ const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...`
 
-    let valor = 'Matheus Lima'
-
-
     const aoDigitado = (evento) => {
-        valor = evento.target.value
-        console.log(valor)
+        props.aoAlterado(evento.target.value)
     }
 
     return(
@@ -17,7 +13,7 @@ const CampoTexto = (props) => {
             <label>
                 {props.label}
             </label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
---
+++ src/componentes/Time/index.js
@@ -0,0 +1,9 @@
+import './Time.css'
+
+const Time = () => {
+    return(
+        <><div><button>Reiniciar</button><button>-</button><button>+</button></div></>
+    )
+}
+
+export default Time
